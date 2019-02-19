1fec1b53941b378f55a16d90fe5497cf7f0d9169
League <league@WTS8.attlocal.net>
2019-02-18 18:28:02 -0800

    start of section 2... nasa countdown.

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,14 +3,14 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
-  * Somebody has to do the countdown – or the rocket won’t launch.
- * 
- * The ground control chief will tell you where to start – then count down to 0. 
- * 
- * (example for ’11’: 11 10 9 8 7 6 5 4 3 2 1 0)
- * 
- **/
+/* NASA wants to shoot a rocket into orbit. 
+ * Somebody has to do the countdown – or the rocket won’t launch.
+* 
+* The ground control chief will tell you where to start – then count down to 0. 
+* 
+* (example for ’11’: 11 10 9 8 7 6 5 4 3 2 1 0)
+* 
+**/
 
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
@@ -19,21 +19,22 @@ public class NasaCountdown {
 		// 3. Change the countdown to use the new starting point
 		
 		// 1. Print a countdown from 10 to 0 on the console
-	
-			// 4. Use the speak method to hear the countdown.
-		
-			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-		
+		for (int i = 10; i < 1; i--) {
+			System.out.println(i);
+		}
+		// 4. Use the speak method to hear the countdown.
+
+		// 6. Use the following code to make the program wait one second for each
+		// number: Thread.sleep(1000);
+
 		// 5. when the counting is done, speak "blastoff!"
 	}
 
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
