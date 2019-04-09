58c80f46d1860a607935333fcfefa755ea40f4ff
League <league@WTS8.attlocal.net>
2019-04-08 18:25:59 -0700

    finishing section 2.
    me is = done with randoms and mole hole things.

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,15 +9,35 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			
 		
-		Random randomMaker = new Random();
+		 Random randomMaker = new Random();
 		
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
-		String Y=randomNumber;
-		// 1. Use each value of randomNumber to give the user a random compliment.
+	
+			
 		
+		// 1. Use each value of randomNumber to give the user a random compliment.
+		if (randomNumber==4) {
+			JOptionPane.showMessageDialog(null, "You suck.");
+		}
+		if (randomNumber==3) {
+			JOptionPane.showMessageDialog(null, "You look like my toe.");
+			
+		}
+		if (randomNumber==2) {
+			JOptionPane.showMessageDialog(null, "You're such a wannabe person. person.");
+		}
+		if (randomNumber==1) {
+			JOptionPane.showMessageDialog(null, "No you.");
+		}
+		if (randomNumber==0) {
+			JOptionPane.showMessageDialog(null, "Ur MOM.");
+		}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
